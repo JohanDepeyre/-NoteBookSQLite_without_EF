349ce97a29f43f3788d96acd8eca94bb7d7e8efa
JohanDepeyre <johan.depeyre@gmail.com>
2022-04-13 21:26:55 +0200

    DAO

---
+++ NoteBookSQLite_whitoutEF/classes/Categorie.cs
@@ -0,0 +1,23 @@
+﻿using NoteBookSQLite_whitoutEF.classes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace NoteBookSQLite_whitoutEF
+{
+    class Categorie
+    {
+        private int idCategorie;
+        private string libelleCategorie;
+        public int IdCategorie { get => idCategorie; set => idCategorie=value; }
+        public string LibelleCategorie { get => libelleCategorie; set => libelleCategorie=value; }
+        public Categorie(int idCategorie, string libelleCategorie)
+        {
+            this.idCategorie=idCategorie;
+            this.libelleCategorie=libelleCategorie;
+        }
+
+      
+    }
+}
---
+++ NoteBookSQLite_whitoutEF/classes/CategorieDAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace NoteBookSQLite_whitoutEF.classes
+{
+     class CategorieDAO : NoteBookDao<Categorie>
+    {
+        public override object find(int id)
+        {
+            var query = "SELECT * FROM Categorie WHERE Id = @id";
+
+            var args = new Dictionary<string, object>
+    {
+        {"@id", id}
+    };
+
+            DataTable dt = base.Execute(query, args);
+
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            var categorie = new Categorie(Convert.ToInt32(dt.Rows[0]["idCategorie"]), Convert.ToString(dt.Rows[0]["nomCategorie"]));
+   
+            return categorie;
+        }
+
+    }
+
+}
